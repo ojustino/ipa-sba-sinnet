dc3b1c46dc001acdda65d3b2c480614194476d9b
O . O <ojustino@users.noreply.github.com>
2021-12-31 20:13:01 -0500

    Diagnosing Mac test failure

--- binder/actions_installs.sh
+++ binder/actions_installs.sh
@@ -8,7 +8,7 @@ if [ "$RUNNER_OS" == "Linux" ]; then
     wget --no-verbose -O /tmp/chromedriver-64b.zip https://chromedriver.storage.googleapis.com/95.0.4638.69/chromedriver_linux64.zip
     # last worked with v86.0.4240.22 on Travis
 elif [ "$RUNNER_OS" == "macOS" ]; then
-    wget --no-verbose -O /tmp/chromedriver-64b.zip https://chromedriver.storage.googleapis.com/95.0.4638.69/chromedriver_mac64.zip
+    wget --no-verbose -O /tmp/chromedriver-64b.zip https://chromedriver.storage.googleapis.com/93.0.4577.63/chromedriver_mac64.zip
     # last worked with v83.0.4103.39 on Travis
 else
     echo "Invalid OS. Windows TBD..."
@@ -30,8 +30,8 @@ fi
 tar -xzf /tmp/geckodriver-v0.23.0.tar.gz -C ~/bin/
 echo "----^geckodriver untarred^----"
 
-# add *drivers' location to path so selenium can find them
-export PATH="$PATH:~/bin/"
+# ~/bin must be added to PATH, but the export PATH method doesn't persist across
+# jobs in GitHub Actions! set it in the .yml file instead.
 
 # install (Linux)/download and unpack (Mac) chromium
 if [ "$RUNNER_OS" == "Linux" ]; then
@@ -45,13 +45,14 @@ if [ "$RUNNER_OS" == "Linux" ]; then
     sudo apt-get --yes -qq install chromium-browser
     chromium-browser --version
 elif [ "$RUNNER_OS" == "macOS" ]; then
-    # uninstall Chrome so it's not chosen by selenium over chromium
+    # uninstall default Chrome and chromedriver to avoid confusion in selenium
     ls -d /Applications/Go*
     echo "--^/Applications/Go*^--"
-    sudo rm -r '/Applications/Google Chrome.app' '~/Library/Application Support/Google/Ch*'
+    sudo rm -r '/Applications/Google Chrome.app' ~/Library/Application Support/Google/Ch* /usr/local/bin/chromedriver
     # wget --no-verbose -O /tmp/chromium-83.0.4103.97.zip https://github.com/macchrome/macstable/releases/download/v83.0.4103.97-r756066-Ungoogled-macOS/Chromium.app.ungoogled-83.0.4103.97.zip
-    wget  --no-verbose -O /tmp/chromium-95.0.4638.69.zip https://github.com/macchrome/macstable/releases/download/v95.0.4638.69-r920003-macOS/Chromium.95.0.4638.69.sync.app.zip
-    unzip -q /tmp/chromium-95.0.4638.69.zip -d /Applications
+    # wget  --no-verbose -O /tmp/chromium-95.0.4638.54.zip https://github.com/macchrome/macstable/releases/download/v95.0.4638.54-r920003-Ungoogled-macOS/Chromium.app.ungoogled-95.0.4638.54.zip
+    wget  --no-verbose -O /tmp/chromium-93.0.4577.63.zip https://github.com/macchrome/macstable/releases/download/v93.0.4577.63-r902210-Ungoogled-macOS/Chromium.app.ungoogled-93.0.4577.63.zip
+    unzip -q /tmp/chromium-93.0.4577.63.zip -d /Applications
 else
     echo "Invalid OS. Windows TBD..."
 fi
@@ -63,6 +64,8 @@ echo "----^chromium equipped^----"
 ls /tmp/
 echo "--------^/tmp/^--------"
 ls ~/bin
-echo "--------^/bin/^--------"
+echo "-------^~/bin/^--------"
 if [ "$RUNNER_OS" == "macOS" ]; then ls /App*; else ls -d /usr/bin/ch* /usr/bin/go*; fi
 echo "--------^apps^---------"
+ls /usr/local/bin/ch*
+echo "-phantom-chromedriver?-"
